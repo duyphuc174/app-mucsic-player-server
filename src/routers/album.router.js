a991fd8d8aedba81a809aebb52289b1d166c1895
duyphuc174 <nguyenduyphuc.174@gmail.com>
2023-04-05 09:54:56 +0700

    thêm song vào album 2

--- src/routers/album.router.js
+++ src/routers/album.router.js
@@ -7,6 +7,7 @@ router.get('/:id', albumController.showById);
 router.post('/create', albumController.create);
 router.delete('/:id', albumController.delete);
 router.put('/:id', albumController.update);
+router.put('/:id/add-song', albumController.addSong);
 router.get('/', albumController.showAll);
 
 module.exports = router;
