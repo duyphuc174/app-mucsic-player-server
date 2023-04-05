2bd4c6e311caa6f4477cb134cff305b92539d65d
duyphuc174 <nguyenduyphuc.174@gmail.com>
2023-04-05 09:52:39 +0700

    thêm song vào album

--- src/app/controllers/AlbumController.js
+++ src/app/controllers/AlbumController.js
@@ -46,6 +46,20 @@ class AlbumController {
             .then(() => res.json({ message: 'Sửa album thành công!' }))
             .catch(next);
     }
+
+    // [PUT] /albums/:id/add-song
+    async addSong(req, res, next) {
+        const album = await Album.findOne({ _id: req.params.id });
+        const songs = album.songs;
+        const songId = req.body._id;
+        if (songs.includes(songId)) {
+            return res.status(400).json({ message: 'Bài hát đã có trong album' });
+        }
+        songs.push(songId);
+        await Album.updateOne({ _id: req.params.id }, songs)
+            .then(() => res.json({ message: 'Bài hát đã được thêm vào album!' }))
+            .catch(next);
+    }
 }
 
 module.exports = new AlbumController();
